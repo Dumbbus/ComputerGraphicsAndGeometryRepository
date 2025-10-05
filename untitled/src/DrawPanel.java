2bb768d4dbdbcab9d5d793da3e3723535067a8d5
Dumbbus <morakovkarsar@gmail.com>
2025-10-05 16:17:51 +0300

    Вот этот перебирает пнгшки

--- untitled/src/DrawPanel.java
+++ untitled/src/DrawPanel.java
@@ -1,17 +1,24 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class DrawPanel extends JPanel implements ActionListener {
-
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
     private final int PANEL_WIDTH;
     private final int PANEL_HEIGHT;
     private final int TIMER_DELAY;
     private Timer timer;
     private int ticksFromStart = 0;
-
-    private Amogus amogus;
+    private int count = 300;
+    private JLabel label = new JLabel();
 
     public DrawPanel(final int width, final int height, final int timerDelay) {
         this.PANEL_WIDTH = width;
@@ -19,22 +26,35 @@ public class DrawPanel extends JPanel implements ActionListener {
         this.TIMER_DELAY = timerDelay;
         timer = new Timer(timerDelay, this);
         timer.start();
-
-        this.amogus = new Amogus(ticksFromStart, 200, 200, 260, Color.RED);
+        add(label);
+        setVisible(true);
+        scheduler.scheduleWithFixedDelay(task, 0, 30, TimeUnit.MILLISECONDS);
     }
 
+    Runnable task = () -> {
+        StringBuilder frame_name = new StringBuilder();
+        frame_name.append("ForApple/frame_");
+        BufferedImage img = null;
+        try {
+            int zero_count = ("" + count).length();
+            if (zero_count < 4){
+                frame_name.append("0".repeat(4 - zero_count));
+            }
+            String no = count + ".png";
+            no = frame_name.append(no).toString();
+            img = ImageIO.read(new File(no));
+            label.setIcon(new ImageIcon(img));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        };
+        count++;
+    };
     @Override
     public void paint(final Graphics gr) {
         super.paint(gr);
-        amogus.setX(ticksFromStart);
-        amogus.draw(gr);
     }
 
     @Override
     public void actionPerformed(final ActionEvent e) {
-        if (e.getSource() == timer) {
-            repaint();
-            ++ticksFromStart;
-        }
     }
 }
--- untitled/src/MainWindow.java
+++ untitled/src/MainWindow.java
@@ -5,7 +5,7 @@ public class MainWindow extends JFrame {
     private final DrawPanel panel;
 
     public MainWindow() throws HeadlessException {
-        panel = new DrawPanel(this.getWidth(), this.getHeight(), 100);
+        panel = new DrawPanel(this.getWidth(), this.getHeight(), 33);
         this.add(panel);
     }
 }
